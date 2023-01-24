f91ee1a84fa2c5eb6d945acc94d56cf07158a46d
Breland <claybreland1@gmail.com>
2023-01-24 15:34:49 -0600

    FSO - API

---
+++ FSO-tutorial/exercises.md
@@ -0,0 +1,26 @@
+## 3.1: Phonebook backend step1 ✅ 
+Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons.
+
+## 3.2: Phonebook backend step2 ✅ 
+Implement a page at the address http://localhost:3001/info that looks roughly like this:
+The page has to show the time that the request was received and how many entries are in the phonebook at the time of processing the request.
+
+## 3.3: Phonebook backend step3 ✅ 
+Implement the functionality for displaying the information for a single phonebook entry. The url for getting the data for a person with the id 5 should be http://localhost:3001/api/persons/5
+If an entry for the given id is not found, the server has to respond with the appropriate status code.
+
+## 3.4: Phonebook backend step4 ✅ 
+Implement functionality that makes it possible to delete a single phonebook entry by making an HTTP DELETE request to the unique URL of that phonebook entry.
+Test that your functionality works with either Postman or the Visual Studio Code REST client.
+
+## 3.5: Phonebook backend step5 ✅ 
+Expand the backend so that new phonebook entries can be added by making HTTP POST requests to the address http://localhost:3001/api/persons.
+Generate a new id for the phonebook entry with the Math.random function. Use a big enough range for your random values so that the likelihood of creating duplicate ids is small.
+
+## 3.6: Phonebook backend step6 ✅ 
+Implement error handling for creating new entries. The request is not allowed to succeed, if:
+* The name or number is missing
+* The name already exists in the phonebook
+Respond to requests like these with the appropriate status code, and also send back information that explains the reason for the error, e.g.:
+
+{ error: 'name must be unique' }
--- FSO-tutorial/index.js
+++ FSO-tutorial/index.js
@@ -1,38 +1,109 @@
-const { response } = require('express')
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv')
+dotenv.config()
 
-let notes = [
+app.use(cors());
+app.use(express.json())
+
+let people = [
     {
-        id: 1,
-        content: "HTML is easy",
-        important: true
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
-]
+];
 
-app.get('/', (request, response) => {
-    response.send('<h1>Helloooooooo!</h1>')
+app.get('/', (req, res) => {
+    res.send('<h1>Hellootttttttoooo!</h1>')
 })
 
+app.get('/api/people', (req, res) => {
+    res.json(people)
+})
+app.get('/api/people/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const person = people.find(person => person.id === id)
+    if (!person) {
+        res.status(404).json({ error: 'Person not found' });
+        return;
+    }
+    res.json(person)
+})
+app.get('/info', (req, res) => {
 
-app.get('/api/notes:id', (req, res) => {
-    response.json(notes)
+    // Get the current date and time
+    const date = new Date();
+
+    // Get the number of entries in the phonebook
+    const count = people.length;
+
+    // Send a response with the date and count
+    res.send(`Phonebook has info for ${count} people<br><br>${date}`);
+});
+
+
+app.post('/api/people', (req, res) => {
+    const body = req.body
+
+    if ((!body.name) || (!body.number)) {
+        return res.status(400).json({
+            error: 'name or number missing from POST request properties <br> send request in JSON fromat like this {"name": "", "number": ""}'
+        })
+    }
+    let existingPerson = people.find(p => p.name === body.name)
+    if (existingPerson) {
+        return res.status(400).json({
+            error: 'database contains that name already; name must be unique'
+        })
+    }
+    const generateId = () => {
+        const maxId = people.length > 0
+            ? Math.max(...people.map(n => n.id))
+            : 0
+        return maxId + 1
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    people = people.concat(person)
+    console.log(people)
+    res.json(people)
+});
+
+
+app.delete('/api/people/:id', (req, res) => {
+    const id = Number(req.params.id)
+    people = people.filter(p => p.id !== id)
+    console.log(people)
+    res.status(204).end()
 })
-app.get('/api/notes:id', (req, res) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    response.json(note)
+// centralized error handling middleware to remove error handling in every endpoint
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    if (!err.statusCode) err.statusCode = 500
+    res.status(err.statusCode).send(err.message)
 })
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
